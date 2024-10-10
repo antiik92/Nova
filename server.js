0929cd75ffead8c99a9d4e74c10aecaf0b93071b
antiik92 <75446017+antiik92@users.noreply.github.com>
2024-10-10 01:19:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ const dotenv = require('dotenv');
 const chatbotRoutes = require('./routes/chatbotRoutes');
 const cors = require('cors');
 const path = require('path');
+const { AzureOpenAI } = require('openai'); 
 
-dotenv.config(); // Carga las variables de entorno al inicio
+dotenv.config();
+
+const client = new AzureOpenAI({
+    endpoint: process.env.AZURE_OPENAI_ENDPOINT,
+    apiKey: process.env.AZURE_OPENAI_API_KEY,
+});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// Servir los archivos estáticos desde 'frontend/public'
 app.use(express.static(path.join(__dirname, '../frontend/public')));
+
+// Ruta para manejar las solicitudes a la raíz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/public/index.html')); // Cambia 'index.html' por tu archivo principal
+});
 
 // Rutas para el chatbot
 app.use('/api', chatbotRoutes);
