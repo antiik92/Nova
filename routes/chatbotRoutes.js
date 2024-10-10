c0eee6d67cb64f525c5ef9d1a5550b45cfd7ad70
antiik92 <75446017+antiik92@users.noreply.github.com>
2024-10-10 00:50:12 -0600

    Update chatbotRoutes.js

--- routes/chatbotRoutes.js
+++ routes/chatbotRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { handleChat } = require('../controllers/chatbotController');
+const { handleChat } = require('../utils/openaiClient');
+
 const router = express.Router();
 
 router.post('/chat', handleChat);
